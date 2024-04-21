f7b24422bf62f0bbabfdd597a927b03b3e3fb12b
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-21 14:52:50 +0200

    3 way to make array

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Array
+﻿namespace Array
 {
 
     class Program
@@ -7,7 +6,36 @@ namespace Array
 
         static void Main(string[] args)
         {
-            
+            // declare and initilize array grades
+            int[] grades = new int[5];
+
+            grades[0] = 20;
+            grades[1] = 15;
+            grades[2] = 12;
+            grades[3] = 9;
+            grades[4] = 22;
+
+            Console.WriteLine("grades at index 0: {0}",grades[0]);
+           
+
+            string input = Console.ReadLine();
+            // assign value to array  grades at index 0
+            grades[0] = int.Parse(input); 
+            Console.WriteLine("grades at index 0: {0}", grades[0]);
+
+            // another way of initializing an array
+            int[] gradesOfMathStudentsA = {20,13,12,8,9,10};
+
+            // thrid way of initiakizing an array
+            int[] gradesOfMathStudentsB = new int[] {20,13,12,8,10};
+
+            Console.WriteLine("Lenght of gradeOfMathStudnesA: {0}", gradesOfMathStudentsA.Length); 
+            Console.ReadLine() ;
+
+
+
+
+
 
         }
 
