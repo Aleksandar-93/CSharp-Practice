57cc3fb7aa23b5c4d3d07076ef7d2f2637f6b84a
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-27 00:10:47 +0200

    studentsTable Hashtable print student

--- Hashtables/Program.cs
+++ Hashtables/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Hashtables
+﻿using System;
+using System.Collections;
+namespace Hashtables
 {
     class Program
     {
@@ -7,8 +9,21 @@
         static void Main(string[] args)
         {
 
+            Hashtable studentsTable = new Hashtable();
 
-            
+            Student stud1 = new Student(1, "Maria", 98);
+            Student stud2 = new Student(2, "Jason", 76);
+            Student stud3 = new Student(3, "Clara", 43);
+            Student stud4 = new Student(4, "Steve", 55);
+
+            studentsTable.Add(stud1.id, stud1);
+            studentsTable.Add(stud2.id, stud2);
+            studentsTable.Add(stud3.id, stud3);
+            studentsTable.Add(stud4.id, stud4);
+
+            Student storedStudent1 = (Student)studentsTable[1];
+
+            Console.WriteLine("Student ID:{0}, Name:{1}, GPA{2}", storedStudent1.id, storedStudent1.name, storedStudent1.GPA);
         }
     }
 
