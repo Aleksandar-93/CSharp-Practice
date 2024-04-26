930ccd609725a02cce3f24c0a16c0871334a71dd
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-26 23:52:00 +0200

    Hashtables created

---
+++ Hashtables/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Hashtables
+{
+    class Program
+    {
+        //Key - value
+
+        static void Main(string[] args)
+        {
+
+
+            
+        }
+    }
+
+    class Student
+    {
+        // orioery called id 
+        public int id { get; set; }
+        //property called name
+        public string name { get; set; }
+        //property called GPA
+        public float GPA { get; set; }
+
+        // simple constructor
+
+        public Student(int id, string name, float GPA) {
+            this.id = id;
+            this.name = name;
+            this.GPA = GPA;
+        }
+    }
+}
