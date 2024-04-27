0a6155d41ede019207f156fcab350784d76bb3fb
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-27 23:09:12 +0200

    print order

--- Queus/Program.cs
+++ Queus/Program.cs
@@ -7,23 +7,59 @@ namespace Queue
     {
         static void Main(string[] args)
         {
-            // Defining a queue of intigers
-            Queue<int> queue = new Queue<int>();
-            queue.Enqueue(1);
-            Console.WriteLine("The value at the front of the que is : {0}", queue.Peek());
-            queue.Enqueue(2);
-            Console.WriteLine("The value at the front of the que is : {0}", queue.Peek()); 
-            queue.Enqueue(3);
-            Console.WriteLine("The value at the front of the que is : {0}", queue.Peek());
-
-            while (queue.Count > 0)
+            
+            Queue<Order> ordersQueue = new Queue<Order>();
+            foreach (Order o in RecieveOrdersFromBranch1())
             {
-                //Dequeue() will return the element that was removed from te que
-                Console.WriteLine("The front value {0} was removed from the queue", queue.Dequeue());
-                //print the que count
-                Console.WriteLine("Current que count is : {0}", queue.Count);
+                //add each order to the queue
+                ordersQueue.Enqueue(o);
             }
+
+            foreach (Order o in RecieveOrdersFromBranch2())
+            {
+                //add each order to the queue
+                ordersQueue.Enqueue(o);
+            }
+
+            while (ordersQueue.Count > 0)
+            {
+                //remove the order at the front of the queue
+                //and store it in a variable called currentOrder
+                Order currentOrder = ordersQueue.Dequeue();
+                //process the order
+                currentOrder.ProcessOrder();
+            }
+
         }
+
+        // this method will create an array of orders an return it
+        static Order[] RecieveOrdersFromBranch1()
+        {
+            //creating new orders array
+            Order[] orders = new Order[]
+            {
+                new Order(1,5),
+                new Order(2,4),
+                new Order(6,10),
+            };
+            return orders;
+
+        }
+
+        static Order[] RecieveOrdersFromBranch2()
+        {
+            //creating new orders array and initializing it with some objects of type Order
+            Order[] orders = new Order[]
+            {
+                new Order(3,5),
+                new Order(4,4),
+                new Order(5,10),
+            };
+            //return the array of orhers that we created
+            return orders;
+
+        }
+
     }
 
     class Order
