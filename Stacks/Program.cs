e7fe2892f34cb87a04629c78ef89f015dbd3568f
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-27 16:44:18 +0200

    item Pop()

--- Stacks/Program.cs
+++ Stacks/Program.cs
@@ -10,10 +10,18 @@ namespace Stacks
             //Define a new stack 
             Stack<int> stack = new Stack<int>();
             //add elements to the stack using Push()
-            stack.Push(1);
-            stack.Push(22);
             //Peek() will retur the element at the top of the stack withaut removing it
+
+            stack.Push(1);
             Console.WriteLine("Top value in the stack is : {0}", stack.Peek());
+            stack.Push(2);
+            Console.WriteLine("Top value in the stack is : {0}", stack.Peek());
+            stack.Push(3);
+            // remove item Pop() last item added
+            int myStackItem = stack.Pop();
+            Console.WriteLine("Popped Item : {0}", myStackItem);
+            Console.WriteLine("Top value in the stack is : {0}", stack.Peek());
+
 
         }
     }
