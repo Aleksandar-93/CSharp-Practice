3194702cf4136bfdb27421170e97e7d4692aad2b
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-27 16:36:50 +0200

    Stacks created

---
+++ Stacks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define a new stack 
+            Stack<int> stack = new Stack<int>();
+            //add elements to the stack using Push()
+            stack.Push(1);
+            stack.Push(22);
+            //Peek() will retur the element at the top of the stack withaut removing it
+            Console.WriteLine("Top value in the stack is : {0}", stack.Peek());
+
+        }
+    }
+}
