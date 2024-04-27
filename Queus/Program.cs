d0fead02983ca0333bbd47c2a302a08807eaf7d4
Aleksandar <60047712+Aleksandar-93@users.noreply.github.com>
2024-04-27 17:08:45 +0200

    Defining a queue

---
+++ Queus/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Defining a queue of intigers
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+        }
+    }
+}
